b44f80b604858cce37c11951aec84af29b2ce2e4
paul-hartwich <paulnoahhartwich@gmail.com>
2024-10-19 22:23:13 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-document.getElementById('colorButton').addEventListener('click', function() {
-    const colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#33FFF3'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('copyright-year').textContent = new Date().getFullYear();
 });
-
-function updateDateTime() {
-    const now = new Date();
-    const dateTimeString = now.toLocaleString();
-    document.getElementById('dateTime').textContent = dateTimeString;
-}
-
-// Update the date and time every second
-setInterval(updateDateTime, 1000);
-
-// Initial call to display the date and time immediately on page load
-updateDateTime();
